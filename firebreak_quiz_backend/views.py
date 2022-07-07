53235e8ec74e947ae362668cf6b0a458095c2f1d
marijnkampf <marijn.kampf@digital.trade.gov.uk>
2022-07-07 15:44:22 +0100

    Initial answer API Todo Filter by question

--- firebreak_quiz_backend/views.py
+++ firebreak_quiz_backend/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import QuestionSerializer
-from .models import Question
+from .serializers import AnswerSerializer, QuestionSerializer
+from .models import Answer, Question
 
 # Create your views here.
+
 
 class QuestionView(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
+
+
+# TODO Filter by Question ID
+class AnswerView(viewsets.ModelViewSet):
+    serializer_class = AnswerSerializer
+    queryset = Answer.objects.all()
