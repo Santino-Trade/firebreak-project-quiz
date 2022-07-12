e0b27530aa4ec941f2bd7089f77dddd6cfa45456
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2022-07-12 08:58:28 +0100

    Made the API linkable (#12)

--- firebreak_quiz_backend/serializers.py
+++ firebreak_quiz_backend/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 from .models import Answer, Question
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ("id", "title", "points")
-
-
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ("id", "answer", "correct")
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    # class QuestionSerializer(serializers.ModelSerializer):
+    id = serializers.HyperlinkedIdentityField(view_name="question-detail")
+    answers = serializers.HyperlinkedIdentityField(view_name="question-answers")
+    # answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ("id", "title", "points", "answers")
